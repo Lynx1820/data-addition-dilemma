1eb150c6525e6dbd68b690191f4d4821544af264
Keren Fuentes <kerenfuentes313@gmail.com>
2024-12-20 13:28:20 -0500

    WIP: Adding the encrypted model and scores

--- kl_utils.py
+++ kl_utils.py
@@ -1,32 +1,78 @@
 # KL Distribution Check
 # python kl_utils.py --n_samples 300 --score
 
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
+#from concrete.ml.sklearn import LogisticRegression
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import random
-import scipy.stats
 import os 
 import argparse
 import sys
 from pathlib import Path
-
+#from pytorch_lightning import seed_everything
+import torch 
+import crypten 
+from crypten import CrypTensor
+from jaxtyping import Int, Float
+crypten.init()
+torch.manual_seed(42)
+torch.set_num_threads(1)
 from folktables_exp import metrics as mt
-
+DATA_PATH ='../yaib_logs/eicu/Mortality24/LogisticRegression'
 # CONSTS
 HOSPITAL_IDS = [73, 264, 420, 243, 338, 443, 199, 458, 300, 188, 252, 167]
 
+class EncryptedLogisticRegression(crypten.nn.Module):
+    
+    def __init__(self, feature_dim):
+        super().__init__()
+        self.linear = crypten.nn.Linear(feature_dim, 1)
+        
+    def forward(self, x):
+        return self.linear(x).sigmoid()
+
+def train_model(model, X, y, tol, max_iter=10, learning_rate=0.06):
+    criterion = crypten.nn.BCELoss()
+    prev = None
+    for epoch in range(max_iter): 
+        model.zero_grad()
+        output = model(X)
+        loss = criterion(output.squeeze(), y)
+        if epoch % 10 == 0: 
+            print(f"epoch {epoch} loss: {loss.get_plain_text()}")
+        loss.backward()
+        model.update_parameters(learning_rate)
+        if prev and abs(prev - loss) < tol:
+            print(f"Converged after {epoch} iterations")
+            break
+    return model
+
+class EncryptedStandardScaler(): 
+    ## (x- mean)/std 
+    def fit(self, x):
+         n_samples = x.shape[0]
+         self.mean = x.mean(dim=0) ## samples, features
+         var = (x.sub(self.mean)).pow(2).sum(dim=0).div(n_samples)
+        
+        # numerical instability 
+         constain_zero = var.eq(torch.tensor(0))
+         self.std = var.add(constain_zero).sqrt()
+
+    def transform(self, x):
+        return (x.sub(self.mean)).div(self.std)
+    
 def get_hospital(hid, split='train', max_samples=None, sample_ratio=1, rand_seed=42): 
     log_dir = f'distance_data'
-    file_name =f'{hid}/data.npz'
+    ## change: testing on my data splits 
+    file_name = f'{DATA_PATH}/train{hid}-test{hid}/data.npz' # f'{hid}/data.npz'
     hos = np.load(os.path.join(log_dir, file_name), allow_pickle=True)
     x = hos[split].item()['features']
     y = hos[split].item()['labels']
@@ -111,14 +157,32 @@ def run_hospital_kl_density(n_runs=5, n_samples=2000, n_components=3):
                     print(f"computing {h1} {h2}, kl_xy{KL_xy[run, i, j]}") 
         results['KL_x'] = KL_x
         results['KL_xy'] = KL_xy
-        np.savez(f"YAIB/results/distances/KL-n{n_samples}-c{n_components}-pair.npz", **results)
+        np.savez(f"../YAIB/results/distances/KL-n{n_samples}-c{n_components}-pair.npz", **results)
     return 
 
+def run_encrypted_model(encrypted_fea1 : Float[CrypTensor, "n_samples features"], encrypted_fea2 :  Float[CrypTensor, "n_samples features"]): 
+    features_dim = encrypted_fea1.shape[1]
+    enc_X_train = crypten.cat([encrypted_fea1, encrypted_fea2], dim=0)
+    
+    enc_Y_train = crypten.cryptensor(np.concatenate((np.ones(len(encrypted_fea1)), np.zeros(len(encrypted_fea2))), axis=0))
+    scaler = EncryptedStandardScaler()
+    model = EncryptedLogisticRegression(features_dim).encrypt()
+    scaler.fit(enc_X_train)
+    enc_scaled_X_train = scaler.transform(enc_X_train)
+    model = train_model(model, enc_scaled_X_train, enc_Y_train, tol=.1, max_iter=10, learning_rate=.05)
+    return scaler, model
 
+def get_encrypted_prediction(model, scaler, encrypted_features): 
+    enc_scaled_X_test = scaler.transform(encrypted_features)
+    return model(enc_scaled_X_test)
+
+#@mpc.run_multiprocess(world_size=2)
 def compute_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
 
     results_x = np.zeros((len(hospital_ids), len(hospital_ids)))
     results_xy = np.zeros((len(hospital_ids), len(hospital_ids)))
+    encrypted_results_x = np.zeros((len(hospital_ids), len(hospital_ids)))
+    encrypted_results_xy = np.zeros((len(hospital_ids), len(hospital_ids)))
     print("computing pairwise score distance function")
     for test_i, test_h in enumerate(hospital_ids):
         for i, h in enumerate(hospital_ids):
@@ -126,8 +190,9 @@ def compute_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
             if h != hos:
                 x, y, xy = get_hospital(h, 'train', max_samples=num_samples)
                 x2, y2, xy2 = get_hospital(hos, 'train', max_samples=num_samples)
+                #if test_i != 11 or i != 8: continue
                 scaler = StandardScaler()
-                logistic = LogisticRegression(max_iter=10000, tol=0.1)
+                logistic = LogisticRegression(max_iter=100, tol=0.1)
                 pipe = Pipeline(steps=[("scaler", scaler), ("logistic", logistic)])
 
                 X_train = np.concatenate((x, x2), axis=0)
@@ -142,9 +207,22 @@ def compute_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
                 Y_train = np.concatenate((np.ones(len(xy)), np.zeros(len(xy2))), axis=0)
 
                 pipe.fit(X_train, Y_train)
-
+                
                 _, _, xy_val = get_hospital(h, 'test')
                 results_xy[i, test_i] = pipe.predict_proba(xy_val)[:, 1].mean()
+                
+                ## Encrypted Pipeline
+                encrypted_x1 = crypten.cryptensor(torch.tensor(x))
+                encrypted_x2 = crypten.cryptensor(torch.tensor(x2))
+                scaler, model = run_encrypted_model(encrypted_x1, encrypted_x2)
+                predictions = get_encrypted_prediction(model, scaler, crypten.cryptensor(torch.tensor(x_val))).get_plain_text()
+                encrypted_results_x[i, test_i] = predictions.mean()
+
+                encrypted_xy1 = crypten.cryptensor(torch.tensor(xy))
+                encrypted_xy2 = crypten.cryptensor(torch.tensor(xy2))
+                scaler, model = run_encrypted_model(encrypted_xy1, encrypted_xy2)
+                predictions = get_encrypted_prediction(model, scaler, crypten.cryptensor(torch.tensor(xy_val))).get_plain_text()
+                encrypted_results_xy[i, test_i] = predictions.mean()
 
     save_dir = Path(save_dir)
     with open(save_dir / 'score-xy.npy', 'wb') as f:
@@ -153,9 +231,17 @@ def compute_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
     with open(save_dir / 'score-x.npy', 'wb') as f:
         np.save(f, results_x)
 
-def compute_kl_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
+    with open(save_dir / 'encrypted-score-xy.npy', 'wb') as f:
+        np.save(f, encrypted_results_xy)
+
+    with open(save_dir / 'encrypted-score-x.npy', 'wb') as f:
+        np.save(f, encrypted_results_x)
+
+def compute_kl_score(hospital_ids: list, save_dir: str, num_samples: int=1000, encrypt=False):
     results_x = np.zeros((len(hospital_ids), len(hospital_ids)))
     results_xy = np.zeros((len(hospital_ids), len(hospital_ids)))
+    encrypted_results_x = np.zeros((len(hospital_ids), len(hospital_ids)))
+    encrypted_results_xy = np.zeros((len(hospital_ids), len(hospital_ids)))
     for test_i, test_h in enumerate(hospital_ids):
         for i, h in enumerate(hospital_ids):
             hos = test_h
@@ -181,20 +267,40 @@ def compute_kl_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
                 Y_train = np.concatenate((np.ones(len(xy)), np.zeros(len(xy2))), axis=0)
 
                 pipe.fit(X_train, Y_train)
-
+                
                 _, _, xy_val = get_hospital(h, 'test')
                 r = pipe.predict_proba(xy_val)[:, 1]
                 r = np.clip(r, 0.01, 0.99)
-                s = r / (1 - r)
-                results_xy[i, test_i] = np.log2(s).mean()
+                results_xy[i, test_i] = (np.log2(s)).mean()
+
+                ## Encrypted Pipeline
+                encrypted_x1 = crypten.cryptensor(torch.tensor(x))
+                encrypted_x2 = crypten.cryptensor(torch.tensor(x2))
+                scaler, model = run_encrypted_model(encrypted_x1, encrypted_x2)
+                predictions = get_encrypted_prediction(model, scaler, x_val).get_plain_text()
+                predictions = np.clip(predictions, 0.01, 0.99)
+                s = predictions / (1 - predictions)
+                encrypted_results_x[i, test_i] = (np.log2(s)).mean()
+
+                encrypted_xy1 = crypten.cryptensor(torch.tensor(xy))
+                encrypted_xy2 = crypten.cryptensor(torch.tensor(xy2))
+                scaler, model = run_encrypted_model(encrypted_xy1, encrypted_xy2)
+                predictions = get_encrypted_prediction(model, scaler, xy_val).get_plain_text()
+                predictions = np.clip(predictions, 0.01, 0.99)
+                # log_p = crypten.cryptensor(t_r.log(), ptype=crypten.mpc.arithmetic)
+                # log_q = crypten.cryptensor((1-t_r).log(), ptype=crypten.mpc.arithmetic)
+                s = predictions / (1 - predictions)
+                encrypted_results_xy[i, test_i] = (np.log2(s)).mean()
 
     save_dir = Path(save_dir)
     with open(save_dir / 'KL-ratio-xy-true.npy', 'wb') as f:
         np.save(f, results_xy)
-
     with open(save_dir / 'KL-ratio-x-true.npy', 'wb') as f:
         np.save(f, results_x)
-
+    with open(save_dir / 'encrypted-KL-ratio-x-true.npy', 'wb') as f:
+        np.save(f, encrypted_results_x)
+    with open(save_dir / 'encrypted-KL-ratio-xy-true.npy', 'wb') as f:
+        np.save(f, encrypted_results_xy)
 
 def compute_addition_score(hospital_ids: list, save_dir: str, num_samples: int=1000):
     results_x = np.zeros((len(hospital_ids), len(hospital_ids)))
@@ -229,6 +335,7 @@ def compute_addition_score(hospital_ids: list, save_dir: str, num_samples: int=1
                 X_train = np.concatenate((xy, xyhalf2), axis=0)
                 Y_train = np.concatenate((np.ones(len(xy)), np.zeros(len(xyhalf2))), axis=0)
 
+
                 pipe.fit(X_train, Y_train)
                 _, _, xy_val = get_hospital(h, 'test')
                 results_xy[i, test_i] = (pipe.predict_log_proba(xy_val)[:, 1].mean())
@@ -240,7 +347,8 @@ def compute_addition_score(hospital_ids: list, save_dir: str, num_samples: int=1
     with open(save_dir / 'addition-score-x.npy', 'wb') as f:
         np.save(f, results_x)
 
-def main():
+def main(my_args=tuple(sys.argv[1:])):
+    crypten.init()
     parser = argparse.ArgumentParser(description="Run KL Check")
 
     parser.add_argument('--n_samples', type=int, default=3000,
@@ -249,11 +357,11 @@ def main():
     parser.add_argument('--score', action='store_true', default=False)
     parser.add_argument('--kl', action='store_true', default=False)
     parser.add_argument('--addition-score', action='store_true', default=False)
-
+    parser.add_argument('--seed', default=2024, type=int)
     # Parse the arguments
-    args = parser.parse_args()
+    args, _ = parser.parse_known_args(my_args)
 
-    hospital_file = 'YAIB-cohorts/data/mortality24/eicu/above2000.txt'
+    hospital_file = '../YAIB-cohorts/data/mortality24/eicu/above2000.txt'
     if not os.path.exists(hospital_file):
         raise ValueError(f"The file {hospital_file} does not exist. Clone the our YAIB-cohorts repo (see readme)")
 
